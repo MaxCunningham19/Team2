3b672a0a9427e28a003ca7b01d9dd314791243f5
Bence R <bencethegreat@gmail.com>
2025-02-22 20:56:21 +0000

    Further improved gap

--- src/app/artist/[artist]/page.tsx
+++ src/app/artist/[artist]/page.tsx
@@ -30,7 +30,7 @@ export default async function Page({
 						<Button>Follow</Button>
 					</div>
 				</div>
-				<div className="mx-auto grid grid-cols-3 gap-32 mt-10">
+				<div className="mx-auto mt-10 grid grid-cols-3 gap-x-32 gap-y-20">
 					<ArtCard />
 					<ArtCard />
 					<ArtCard />
