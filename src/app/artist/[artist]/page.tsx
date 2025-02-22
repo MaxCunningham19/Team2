857d54ab2eccd450fa096f8d4fed8357c830dade
Bence R <bencethegreat@gmail.com>
2025-02-22 20:36:29 +0000

    Added art card

---
+++ src/app/artist/[artist]/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+import PearlEarring from "../../../../public/pearl-earring.jpg"
+import { Button } from "~/app/_components/ui/button"
+
+export default async function Page({
+	params,
+}: {
+	params: Promise<{ artist: string }>
+}) {
+	const slug = (await params).artist
+	console.log(slug)
+
+	return (
+		<>
+			<div className="h-80 w-screen bg-primary"></div>
+			<div className="relative">
+				<Image src={PearlEarring} alt="Artist icon" className="absolute -top-32 left-32 h-64 w-64 rounded-full" />
+				<div className="ml-96 p-4">
+					<h1 className="text-5xl font-bold">Placeholder Name</h1>
+
+					<h2 className="flex flex-row gap-x-2 text-xl font-extralight">
+						<span>/{slug}</span>
+						⋅
+						<span>Joined 2 years ago</span>
+					</h2>
+
+					<div className="flex flex-row gap-x-4 pt-2">
+						<Button>Commission</Button>
+						<Button>Follow</Button>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
