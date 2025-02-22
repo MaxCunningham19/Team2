a4de4445d8431c346d195d0a6d291a01375d7eb9
Bence R <bencethegreat@gmail.com>
2025-02-22 20:59:02 +0000

    Added description

--- src/app/_components/art-card.tsx
+++ src/app/_components/art-card.tsx
@@ -3,8 +3,12 @@ import AppleGuy from "../../../public/apple-guy.jpg"
 
 export const ArtCard = () => {
   return (
-    <div className="w-64 h-[32rem] bg-primary">
+    <div className="h-[32rem] w-64 bg-primary">
       <Image src={AppleGuy} alt="Painting" className="w-64" />
+      <div className="p-4 text-primary-foreground">
+        <h3 className="text-2xl font-bold">Untitled 3</h3>
+        <h4 className="italic">$400</h4>
+      </div>
     </div>
   )
 }
