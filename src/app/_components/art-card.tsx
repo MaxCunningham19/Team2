230c3bcb3d82adcc6114b6af4022194f884212df
Bence R <bencethegreat@gmail.com>
2025-02-22 21:00:56 +0000

    Added purchase button

--- src/app/_components/art-card.tsx
+++ src/app/_components/art-card.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image"
 import AppleGuy from "../../../public/apple-guy.jpg"
+import { Button } from "./ui/button"
 
 export const ArtCard = () => {
   return (
-    <div className="h-[32rem] w-64 bg-primary">
+    <div className="h-[32rem] w-64 bg-accent">
       <Image src={AppleGuy} alt="Painting" className="w-64" />
       <div className="p-4 text-primary-foreground">
         <h3 className="text-2xl font-bold">Untitled 3</h3>
         <h4 className="italic">$400</h4>
+        <p>The artist has not provided a description for this piece.</p>
+        <Button className="mt-2">Purchase</Button>
       </div>
     </div>
   )
