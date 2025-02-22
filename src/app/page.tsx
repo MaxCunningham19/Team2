3d9cfafbd50e57948aade4a165ae1255c6833e52
Bence R <bencethegreat@gmail.com>
2025-02-22 12:59:30 +0000

    Edited primary/accent colours, added examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,32 +15,43 @@ export default async function Home() {
           <h1 className="text-5xl font-extralight tracking-tight sm:text-[5rem]">
             novanti
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-accent text-secondary p-4 hover:bg-accent/90"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-primary text-primary-foreground p-4 hover:bg-primary/90"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">First Steps → (Primary)</h3>
               <div className="text-lg">
                 Just the basics - Everything you need to know to set up your
                 database and authentication.
               </div>
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-secondary text-secondary-foreground p-4 hover:bg-secondary/90"
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <h3 className="text-2xl font-bold">Documentation → (Secondary)</h3>
               <div className="text-lg">
                 Learn more about Create T3 App, the libraries it uses, and how
                 to deploy it.
               </div>
             </Link>
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-accent text-accent-foreground p-4 hover:bg-accent/90"
+              href="https://create.t3.gg/en/usage/first-steps"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">First Steps → (Accent)</h3>
+              <div className="text-lg">
+                Just the basics - Everything you need to know to set up your
+                database and authentication.
+              </div>
+            </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
+            <p className="text-2xl">
               {hello ? hello.greeting : "Loading tRPC query..."}
             </p>
           </div>
