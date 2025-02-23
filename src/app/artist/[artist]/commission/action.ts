0b6aec8d2e328f6286e5d28726b8f3a7fbad287e
Max Cunningham <maxzinedine@gmail.com>
2025-02-23 06:58:26 +0000

    update actions

--- src/app/artist/[artist]/commission/action.ts
+++ src/app/artist/[artist]/commission/action.ts
@@ -1,19 +1,62 @@
-import { api } from "~/trpc/server";
-
+import { createClient } from "@/utils/supabase/server";
+import {
+  type CommissionInsert,
+  type MilestoneInsert,
+} from "~/utils/supabase/types";
 export const createCommission = async function (params: {
-  artist_id: string;
-  price: number;
-  desc: string;
+  commission: CommissionInsert;
+  milestones: MilestoneInsert[];
 }) {
-  const { commissionID, milestoneIDs, error } =
-    await api.commission.create.commission({
-      commission: params,
-      milestones: [],
-    });
+  const supabase = await createClient();
+
+  const { data: commissionID, error } = await supabase
+    .from("commission")
+    .insert([params.commission])
+    .select("id")
+    .single();
+
+  if (!!error || commissionID == null) {
+    return { error: error };
+  }
+
+  if (params.milestones.length <= 0) {
+    return { commissionID: commissionID.id as string };
+  }
+
+  const milestonesWithCommissionId = params.milestones.map((milestone) => ({
+    ...milestone,
+    commission_id: commissionID.id as string, // Add commission ID to each milestone
+  }));
+
+  milestonesWithCommissionId.sort((a, b) => {
+    return a.order_id - b.order_id;
+  });
+  const { data: milestoneIDs, error: milesoneError } = await supabase
+    .from("milestones")
+    .insert([milestonesWithCommissionId])
+    .select("id");
+
+  if (!!error) {
+    const { error: deleteError } = await supabase
+      .from("commission")
+      .delete()
+      .eq("id", commissionID);
+
+    return { error: milesoneError, deleted: !deleteError };
+  }
+
+  return {
+    commissionID: commissionID.id as string,
+    milestoneIDs: milestoneIDs as string[] | null,
+  };
+
   return { commissionID, milestoneIDs, error };
 };
 
 export const getUser = async function () {
-  const { user } = await api.account.getUserAndArtist();
-  return user;
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return user?.id ?? "";
 };
