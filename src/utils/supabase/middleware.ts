9d65cd8893601a5c0aacd108e938cd8d964bcafb
Max Cunningham <maxzinedine@gmail.com>
2025-02-23 03:13:53 +0000

    remove commission

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -48,10 +48,6 @@ export const updateSession = async (request: NextRequest) => {
       return NextResponse.redirect(new URL("/sign-in", request.url));
     }
 
-    if (request.nextUrl.pathname.includes("/commission") && !user.error) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
-
     return response;
   } catch (e) {
     // If you are here, a Supabase client could not be created!
