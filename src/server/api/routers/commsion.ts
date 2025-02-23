eb5e014e9327e269eed442593322af1ede2f3383
Max Cunningham <maxzinedine@gmail.com>
2025-02-23 03:39:28 +0000

    fix import

--- src/server/api/routers/commsion.ts
+++ src/server/api/routers/commsion.ts
@@ -2,11 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { createClient } from "@/utils/supabase/client";
-import {
-  MilestoneUpdate,
-  type Commision,
-  type Milestone,
-} from "src/utils/supabase/types";
+import { type Commision, type Milestone } from "src/utils/supabase/types";
 
 export const commisionRouter = createTRPCRouter({
   createCommision: publicProcedure
