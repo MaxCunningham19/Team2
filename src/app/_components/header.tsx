ca9384f425a3db6fce6380ec54e917c61878eeed
Bence R <bencethegreat@gmail.com>
2025-02-23 11:57:36 +0000

    Various aesthetic stuff

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -3,7 +3,6 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { useAuth } from "~/lib/hooks/useAuth";
 
 const navItems = [
   { title: "Home", href: "/" },
@@ -13,8 +12,6 @@ const navItems = [
 
 export default function Header() {
   const pathname = usePathname();
-  const { user } = useAuth()
-  console.log(user)
 
   return (
     <header className="container fixed left-1/2 top-0 z-[5] -translate-x-1/2 transform px-4 pt-8">
@@ -27,32 +24,23 @@ export default function Header() {
 
         {/* Navigation Bar */}
         <div className="w-full max-w-xl">
-          <nav className="flex items-center justify-between rounded-full border-2 border-[#745543]/20 bg-[#e6ccc0]/50 px-6 py-2 backdrop-blur-md">
-            <div className="relative flex items-center gap-4">
-              {navItems.map((item, index) => (
-                <div key={item.title} className="flex items-center">
-                  <Link
-                    href={item.href}
-                    className={`${pathname === item.href ? "text-[#a88a7c]" : ""} relative px-2 py-1 text-[#745543] transition-colors
+          <nav className="flex items-center justify-around rounded-full border-2 border-[#745543]/20 bg-[#e6ccc0]/50 px-6 py-2 backdrop-blur-md">
+            {navItems.map((item, _) => (
+              <div key={item.title} className="flex items-center">
+                <Link
+                  href={item.href}
+                  className={`${pathname === item.href ? "text-[#a88a7c] underline" : ""} relative px-2 py-1 text-[#745543] transition-colors
                       hover:text-[#a88a7c]`}
-                  >
-                    {item.title}
-                  </Link>
-                </div>
-              ))}
-            </div>
-
-            {user ?
-              <Link href="/login">
-                Signed IN!
-              </Link>
-              :
-              <Link href="/login">
-                <Button className="rounded-full bg-[#a88a7c] px-6 text-white hover:bg-[#a88a7c]/90">
-                  Sign In
-                </Button>
-              </Link>
-            }
+                >
+                  {item.title}
+                </Link>
+              </div>
+            ))}
+            <Link href="/login">
+              <Button className="rounded-full bg-[#a88a7c] px-6 text-white hover:bg-[#a88a7c]/90">
+                Sign In
+              </Button>
+            </Link>
           </nav>
         </div>
       </div>
