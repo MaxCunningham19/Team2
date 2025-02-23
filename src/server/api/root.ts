e83cd4b9fa6ff369516bf250fa7580e7aa2da757
Max Cunningham <maxzinedine@gmail.com>
2025-02-23 00:42:10 +0000

    make some commision stuff

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { stripeRouter } from "./routers/stripe/stripe";
+import { commisionRouter } from "./routers/commsion";
 
 /**
  * This is the primary router for your server.
@@ -8,8 +8,8 @@ import { stripeRouter } from "./routers/stripe/stripe";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   stripe: stripeRouter,
+  commision: commisionRouter,
 });
 
 // export type definition of API
