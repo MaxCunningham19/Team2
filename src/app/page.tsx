c3a7c448442c52e562bbee14476af899d7a5ab1f
Max Cunningham <maxzinedine@gmail.com>
2025-02-23 12:11:51 +0000

    remove comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,6 @@ export default async function Home() {
       <SlidingDoorLayout>
         <div className="flex h-screen flex-col items-center justify-center">
           <HomeHero2 />
-          {/*
-          <div className="mx-auto w-full max-w-xl px-4 ">
-            <div className="relative">
-              <input
-                type="text"
-                className="h-12 w-full rounded-lg border border-gray-200 pl-4 pr-10 focus:border-gray-400 focus:outline-none"
-                placeholder="Search..."
-              />
-              <Search className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400" size={20} />
-            </div>
-          </div>
-          */}
         </div>
       </SlidingDoorLayout>
     </main>
