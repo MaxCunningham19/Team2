25bb021922900874f20ee30389ba180bc744f1ac
Max Cunningham <maxzinedine@gmail.com>
2025-02-23 06:19:32 +0000

    fix error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-export default function ErrorPage()
-{
-    return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>Oops! Something went wrong.</h1>
-            <p>We're sorry, but the page you were looking for doesn't exist.</p>
-            <a href="/">Go back to the homepage</a>
-        </div>
-    );
-};
+export default function ErrorPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Oops! Something went wrong.</h1>
+      <p>
+        We&apos;re sorry, but the page you were looking for doesn&apos;t exist.
+      </p>
+      <Link href="/">Go back to the homepage</Link>
+    </div>
+  );
+}
