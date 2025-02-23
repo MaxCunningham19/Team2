96fb1a11139333ecb06cb6d7ccf984bb4abd4db5
Max Cunningham <maxzinedine@gmail.com>
2025-02-23 12:43:52 +0000

    make endpoint

--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -39,6 +39,12 @@ export const accountRouter = createTRPCRouter({
     return { user: userData, artist: artist };
   }),
 
+  getUserID: publicProcedure.query(async () => {
+    const supabase = createClient();
+    const user = await supabase.auth.getUser();
+    return { userID: user.data.user?.id };
+  }),
+
   getArtist: publicProcedure
     .input(z.object({ artist_id: z.string() }))
     .query(async ({ input }) => {
